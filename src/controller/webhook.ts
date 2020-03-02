d38f8caea01464899ce34bc2e16e0fd260aaa7e2
surachet ngewtaisong <surachet.nts@gmail.com>
2020-03-02 14:57:19 +0700

    Update webhook.ts

--- src/controller/webhook.ts
+++ src/controller/webhook.ts
@@ -1,9 +1,13 @@
 import * as express from 'express';
 import * as HttpStatus from 'http-status-codes';
 
+function message_list(message) {
+
+}
+
 export async function webHook(req: express.Request, res: express.Response, next: express.NextFunction) {
     try {
-        console.log(req.body);
+        console.log(req.body.events[0]);
         return res.status(HttpStatus.OK).send();
     } catch (err) {
         console.error(err);
