cb9cf1e9a9e5facec291856e0322adbb3d48c7d2
surachet ngewtaisong <surachet.nts@gmail.com>
2020-03-02 14:37:59 +0700

    test log

--- build/controller/webhook.js
+++ build/controller/webhook.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const HttpStatus = require("http-status-codes");
 async function webHook(req, res, next) {
     try {
+        console.log(req.body);
+        return res.status(HttpStatus.OK).send();
     }
     catch (err) {
         console.error(err);
--- src/controller/webhook.ts
+++ src/controller/webhook.ts
@@ -3,7 +3,8 @@ import * as HttpStatus from 'http-status-codes';
 
 export async function webHook(req: express.Request, res: express.Response, next: express.NextFunction) {
     try {
-        
+        console.log(req.body);
+        return res.status(HttpStatus.OK).send();
     } catch (err) {
         console.error(err);
         return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send();
